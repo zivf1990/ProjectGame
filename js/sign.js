0f3b20e5b6ba758c7eebae73885958c74181fcba
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 <fisher.ziv@gmail.com>
2022-11-22 16:32:47 +0200

    v4

--- js/sign.js
+++ js/sign.js
@@ -1,6 +1,6 @@
+const toggleBtn = document.getElementById("toggle-btn");
 const loginBtn = document.getElementById("toggle-login");
 const signupBtn = document.getElementById("toggle-signup");
-const toggleDiv = document.getElementById("toggle-div");
 const loginForm = document.forms["login-form"];
 const signupForm = document.forms["signup-form"];
 // loginForm["user-id"].value       ("user-id" - (name property))
@@ -11,10 +11,10 @@ signupBtn.addEventListener("click", signup);
 function signup() {
   loginForm.style.left = "-400px";
   signupForm.style.left = "50px";
-  z.style.left = "110px";
+  toggleBtn.style.left = "110px";
 }
 function login() {
-  x.style.left = "50px";
+  loginForm.style.left = "50px";
   signupForm.style.left = "450px";
-  z.style.left = "0px";
+  toggleBtn.style.left = "0px";
 }
