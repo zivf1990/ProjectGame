7e2978f953d0fcbc56a729de829e475fad288bb7
Inon Shachar <inonsh01@gmail.com>
2022-11-23 12:44:06 +0200

    v8

---
+++ js/signin.js
@@ -0,0 +1,39 @@
+
+const logInSubmit = document.getElementById("submit-login-btn");
+
+logInSubmit.addEventListener("click", logInCheck);
+
+function Player(x,y){
+  this.x=x;
+  this.y=y;
+}
+
+
+function logInCheck(event) {
+  let flag = 0;
+  let username = loginForm["user-id"].value;
+  let password = loginForm["password"].value;
+  const usersSignIn = JSON.parse(localStorage.getItem("users"));
+  if(username.length == 0){
+    alert("your user name or password are invalid");
+  }
+  for (let i = 0; i < usersSignIn.length; i++) {
+    console.log(usersSignIn[i].username);
+    console.log(username,password);
+    if (username == usersSignIn[i].username) {
+      if (password == usersSignIn[i].password) {
+        event.preventDefault();
+        welcomePage();
+        flag = 1;
+      }
+    }
+  }
+  if (flag == 0) {
+    alert("your user name or password are invalid");
+  }
+}
+
+function welcomePage() {
+  location.replace("../pages/welcomePage.html");
+}
+
